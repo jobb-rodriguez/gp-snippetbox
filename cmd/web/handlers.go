0e5254dbcc8e635cbc0bd341bf416b08fb88d533
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2026-01-26 06:13:44 +0800

    refactor: directory structure

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -36,17 +35,4 @@ func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
 	msg := fmt.Sprintf("Save a new snippet...")
 	w.Write([]byte(msg))
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-
-	log.Print("starting server on :4000")
-
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
 }
